ef42251486c584fe78318d2b452bd9689de49878
Emma Green <green.emma.2003@gmail.com>
2024-03-20 14:29:16 -0400

    fixing the radius atribute of the circle class

--- Circle.java
+++ Circle.java
@@ -1,3 +1,13 @@
 public class Circle{
-	public int radius;
+	private double radius;
+
+	public void setRadius(double nr) {
+		if(nr > 0) {
+			radius = nr;
+		}
+}
+public double getRadius() {
+	return radius;
+
+	}
 }
